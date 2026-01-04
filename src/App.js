81ed0e3f434532fa346079a0f4b069c71f735edf
AnkitRoyy <ankitkumarroy2007@gmail.com>
2026-01-04 12:44:39 +0530

    fixed scroll position when navigating between pages

--- src/App.js
+++ src/App.js
@@ -9,15 +9,17 @@ import ISDC from "./pages/ISDC";
 import Blogs from "./pages/Blogs";
 import BlogPost from "./pages/BlogPost";
 import Footer from "./components/Footer";
+import ScrollToTop from "./utils/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop/>
+
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/isdc" element={<ISDC />} />
-        <Route path="/blogs" element={<Blogs />} />
         <Route path="/blogs" element={<Blogs />} />
         <Route path="/blogs/:slug" element={<BlogPost />} />
       </Routes>
